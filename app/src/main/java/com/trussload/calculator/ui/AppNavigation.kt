e60ce75956b4fedbf8975e578a6eaea846594ec7
karaboksbaraboks-pixel <karaboksbaraboks@gmail.com>
2026-09-19 03:46:42 +0300

    Update AppNavigation.kt

--- app/src/main/java/com/trussload/calculator/ui/AppNavigation.kt
+++ app/src/main/java/com/trussload/calculator/ui/AppNavigation.kt
@@ -1,8 +1,21 @@
 package com.trussload.calculator.ui
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -15,6 +28,7 @@ enum class AppScreen {
 fun AppNavigation(
     calculatorContent: @Composable () -> Unit
 ) {
+
     var currentScreen by remember {
         mutableStateOf(AppScreen.CALCULATOR)
     }
@@ -79,7 +93,7 @@ fun AppNavigation(
                 }
 
                 AppScreen.ASSEMBLY -> {
-                    TrussAssemblyScreen()
+                    AssemblyWorkspaceScreen()
                 }
             }
         }
