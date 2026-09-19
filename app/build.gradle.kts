cfbfbf20fe89fce6d2d6f5dc0f039e82ad3be5f4
karaboksbaraboks-pixel <karaboksbaraboks@gmail.com>
2026-09-19 03:05:29 +0300

    Increase Android versionCode to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,10 +10,12 @@ android {
 
     defaultConfig {
         applicationId = "com.trussload.calculator"
+
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+
+        versionCode = 2
+        versionName = "1.1"
     }
 
     compileOptions {
@@ -33,7 +35,11 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.0")
-    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
+
+    implementation(
+        platform("androidx.compose:compose-bom:2025.02.00")
+    )
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
